c47e3a9e9278056792da27448629ded438341103
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2023-11-19 18:45:56 +0530

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDB0WNzBWdPyCzetHtNLevAMHU6pSESMEY",
+  authDomain: "qr-code-generator-108e8.firebaseapp.com",
+  projectId: "qr-code-generator-108e8",
+  storageBucket: "qr-code-generator-108e8.appspot.com",
+  messagingSenderId: "507948660496",
+  appId: "1:507948660496:web:e20996cf592c09bfbe5795",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import downloadjs from "downloadjs";
+import html2canvas from "html2canvas";
+import { ChangeEvent, useState } from "react";
+import QRCode from "react-qr-code";
+
+function App() {
+  const [link, setLink] = useState("");
+
+  const handleLinkChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLink(event.target.value);
+  };
+
+  const handleCaptureClick = async () => {
+    const pricingTableElmt = document.querySelector<HTMLElement>("#qrcode");
+    if (!pricingTableElmt) return;
+
+    const canvas = await html2canvas(pricingTableElmt);
+    const dataURL = canvas.toDataURL("image/png");
+    downloadjs(dataURL, "download.png", "image/png");
+  };
+
+  return (
+    <div className="flex items-center flex-col justify-center">
+      <h1 className="flex text-white text-5xl mt-[20px]">QR-CODE GENERATOR</h1>
+
+      <div className="flex flex-col mt-[200px]  items-center justify-center bg-slate-200 w-[70%] py-[30px] rounded-md">
+        <div className="flex flex-col w-[75%] ">
+          <p className="text-black">Enter link</p>
+          <input
+            value={link}
+            className="py-[20px] w-[100%] px-[20px] text-black rounded-lg"
+            onChange={handleLinkChange}
+          ></input>
+          <div className="mt-[20px]">
+            <div
+              id="qrcode"
+              style={{
+                height: "auto",
+                margin: "0 auto",
+                maxWidth: 128,
+                width: "100%",
+              }}
+            >
+              <QRCode
+                size={512}
+                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                value={link}
+                viewBox={`0 0 512 512`}
+              />
+            </div>
+          </div>
+
+          <button
+            className="text-white p-[20px] bg-slate-900 mt-[20px] rounded-lg"
+            onClick={handleCaptureClick}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+      <div className="absolute bottom-0 mb-3">
+        <div>
+          Made by Mohammed Abid Nafi and this project is open sourced and
+          available in my{" "}
+          <a
+            href="https://github.com/MohammedAbidNafi"
+            className="text-blue-400 cursor-pointer"
+          >
+            Github
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
